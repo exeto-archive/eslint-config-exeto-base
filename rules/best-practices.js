11d24e5d5d258c573a592749887a968efda3e370
Timofey Dergachev <t@exeto.me>
2016-10-17 21:55:33 +0300

    Disable 'consistent-return'

--- rules/best-practices.js
+++ rules/best-practices.js
@@ -19,9 +19,6 @@ module.exports = {
       exceptMethods: [],
     }],
 
-    // require return statements to either always or never specify values
-    'consistent-return': 'error',
-
     // specify curly brace conventions for all control statements
     curly: ['error', 'multi-line'],
 
